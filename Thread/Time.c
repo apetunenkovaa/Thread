191bfe45e010d721985441df676da8d60ce22a81
Анастасия Петуненкова <apetunenkovaa@mail.ru>
2022-12-12 17:54:36 +0300

    l

--- Thread/Time.c
+++ Thread/Time.c
@@ -1,31 +1,92 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <Windows.h>
-#include <stdio.h>
-#include <malloc.h>
-HANDLE	hThread[4];
-int main() 
-{
-	system("chcp 1251>nul");
-
-	hThread[0] = CreateThread(NULL, 0, hhh, 0, 0);
-
-
-	printf("¬ведеите номер задачи: \n 1 - включение секундомера \n 2 - включение таймера \n 3 - просмотр часов \n 4 - пауза \n 5 - сн€ть с паузы");
-	int z = 0;
-	switch (z)
-	{
-	case 1:
-		break;
-	case 2:
-		break;
-	case 3:
-		break;
-	case 4:
-		break;
-	case 5:
-		break;
-	default:
-		break;
-	}
-
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <Windows.h>
+//#include <stdio.h>
+//#include <malloc.h>
+//HANDLE	hThread[4];
+//
+//SYSTEMTIME syst_time;
+//int hour, minute, second, millisecond = 0; //основные
+//int hour_SW, minute_SW, second_SW, millisecond_SW = 0;//дл€ секундомера
+//int hour_T, minute_T, second_T, millisecond_T = 0;//дл€ таймера
+//
+//void clock(int i) //часы
+//{
+//	while (1) 
+//	{
+//		switch (n)
+//		{
+//		case 1:
+//			GetLocalTime(&syst_time);
+//			break;
+//		case 2:
+//			second++;
+//			if (second == 60) 
+//			{
+//				minute++;
+//				second = 0;
+//			}
+//			if (minute == 60)
+//			{
+//				hour++;
+//				minute = 0;
+//			}
+//		default:
+//			break;
+//		}
+//		
+//	}
+//}
+//
+//void showClock(int i) //вывод часов
+//{
+//	while(1) {
+//		printf("%d:%d:%d");
+//	}
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//int main() 
+//{
+//	system("chcp 1251>nul");
+//
+//	
+//
+//	printf("¬ведеите номер задачи: \n 1 - включение секундомера \n 2 - включение таймера \n 3 - просмотр часов \n 4 - пауза \n 5 - сн€ть с паузы");
+//	int z = 0;
+//	switch (z)
+//	{
+//	case 1:
+//		break;
+//	case 2:
+//		break;
+//	case 3:
+//		break;
+//	case 4:
+//		break;
+//	case 5:
+//		break;
+//	default:
+//		break;
+//	}
+//
+//}
